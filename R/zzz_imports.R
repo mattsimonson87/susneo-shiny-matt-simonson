e2a227cfb400f35503e69e70409a1281b511822f
Matthew Simonson <mattsimonson419@gmail.com>
2025-09-15 10:54:10 -0500

    fix(check): import shiny/plotly symbols; add DT to Imports
    - DESCRIPTION: add DT; declare Depends: R (>= 4.1.0)
    - Package docs: import shiny::req/validate/need and plotly::plot_ly/layout
    - Also import R6Class, lubridate, stringr helpers to silence unused-Imports warning
    - Re-document; R CMD check down to clean WARNs/NOTEs

--- R/zzz_imports.R
+++
@@ -1,8 +0,0 @@
-#' @keywords internal
-#' @importFrom R6 R6Class
-#' @importFrom dplyr filter count distinct
-#' @importFrom readr read_csv parse_date cols col_character locale parse_datetime
-#' @importFrom plotly plot_ly layout renderPlotly plotlyOutput
-#' @importFrom tidyr pivot_wider
-#' @importFrom readxl read_excel
-NULL
